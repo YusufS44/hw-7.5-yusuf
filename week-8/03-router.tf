068851a9347ca04d910e2793d07afd618ebfc278
YusufS44 <yusuf.sadruddin@gmail.com>
2026-05-07 23:16:21 -0400

    Thursday Commit

--- week-8/03-router.tf
+++ week-8/03-router.tf
@@ -3,10 +3,6 @@ resource "google_compute_router" "router" {
   region  = "us-central1"
   network = google_compute_network.compute_network91.id
 
-  #bgp {
-   # asn = 64514
-  #}
-
   depends_on = [
     google_compute_network.compute_network91
   ]
